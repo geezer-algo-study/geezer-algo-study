5e8c88122d1b5b00479303c88af028542391a4ef
Soberanalysts <106496733+Soberanalysts@users.noreply.github.com>
2024-10-02 23:20:54 +0900

    Revert "[2024.10.01] 나상현 Binary_Search "

--- solution/Binary_Search.js
+++
@@ -1,9 +0,0 @@
-var search = function(nums, target) {
-    let index = -1;
-   for (let i = 0; i < nums.length; i++) {
-       if (nums[i] === target) {
-           index = i;
-       }
-   }
-   return index;
-};
