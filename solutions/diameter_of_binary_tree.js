a88d8eb488bcf0f2163863e79dd8bf9f36078ae3
bada308 <203190@jnu.ac.kr>
2024-10-14 16:36:43 +0900

    fix: diameter of binary tree 코드 수정

--- solutions/diameter_of_binary_tree.js
+++ solutions/diameter_of_binary_tree.js
@@ -8,27 +8,24 @@
  */
 
 /**
- * @param {TreeNode} curr
- * @param {number} depth
- * @return {number}
- */
-const getLength = (curr, depth) => {
-  if (curr === null) return depth;
-
-  lengths = [];
-  lengths.push(getLength(curr.left, depth + 1));
-  lengths.push(getLength(curr.right, depth + 1));
-
-  return Math.max(...lengths);
-};
-
-/**
  * @param {TreeNode} root
  * @return {number}
  */
 var diameterOfBinaryTree = function (root) {
-  const left = getLength(root.left, 0);
-  const right = getLength(root.right, 0);
+  let maxLength = 0;
 
-  return left + right;
+  const getHeight = (node) => {
+    if (node === null) return 0;
+
+    const leftHeight = getHeight(node.left);
+    const rightHeight = getHeight(node.right);
+
+    maxLength = Math.max(maxLength, leftHeight + rightHeight);
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  };
+
+  getHeight(root);
+
+  return maxLength;
 };
