818df39cfd4acebd2cc4ef010d7c51f20e2b67d9
GuNyangE <noel7145@gmail.com>
2024-10-15 11:41:15 +0900

    [Fixed] 두 번째 방법 추가

--- solutions/876_Middle_of_the_Linked List.js
+++ solutions/876_Middle_of_the_Linked List.js
@@ -30,3 +30,40 @@ var middleNode = function (head) {
 
 	return node;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+	if (head.next === null) return head;
+
+	let stackSlower = [];
+	let stackFaster = [];
+	let nodeFaster = head;
+	let nodeSlower = head;
+
+	while (nodeFaster !== null) {
+		stackFaster.push(nodeFaster);
+		stackSlower.push(nodeSlower);
+		nodeFaster = nodeFaster.next;
+		nodeSlower = nodeSlower.next;
+
+		if (nodeFaster !== null) {
+			stackFaster.push(nodeFaster);
+			nodeFaster = nodeFaster.next;
+		} else {
+			nodeSlower = stackSlower[stackSlower.length - 1];
+			break;
+		}
+	}
+
+	return nodeSlower;
+};
