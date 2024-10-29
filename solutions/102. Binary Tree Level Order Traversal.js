57bb573fef94a1ff4493e2ec7b2c50fcccda3ac1
GuNyangE <noel7145@gmail.com>
2024-10-29 14:11:11 +0900

    Failed

--- solutions/102. Binary Tree Level Order Traversal.js
+++ solutions/102. Binary Tree Level Order Traversal.js
@@ -11,43 +11,43 @@
  * @return {number[][]}
  */
 var levelOrder = function (root) {
-	console.log(bfs(root));
-	// let arr = [];
-	// let depth = Math.floor(Math.log2(root.length));
+  console.log(bfs(root));
+  // let arr = [];
+  // let depth = Math.floor(Math.log2(root.length));
 
-	// for(let i = 0; i < depth; i++) {
-	//     let nodeArr = [];
+  // for(let i = 0; i < depth; i++) {
+  //     let nodeArr = [];
 
-	//     for(let j = 0; j < Math.pow(2, i); i++) {
-	//         nodeArr.push(root.shift());
-	//     }
+  //     for(let j = 0; j < Math.pow(2, i); i++) {
+  //         nodeArr.push(root.shift());
+  //     }
 
-	//     arr.push(nodeArr);
-	// }
+  //     arr.push(nodeArr);
+  // }
 
-	// return arr;
-	// return [[1, 2]]
+  // return arr;
+  // return [[1, 2]]
 };
 
 function bfs(root) {
-	let visited = [];
-	let nodeArr = [];
+  let visited = [];
+  let nodeArr = [];
 
-	nodeArr.push([root, 0]);
+  nodeArr.push([root, 0]);
 
-	while (nodeArr.length !== 0) {
-		let tempNode = nodeArr.shift();
-		console.log(tempNode);
+  while (nodeArr.length !== 0) {
+    let tempNode = nodeArr.shift();
+    console.log(tempNode);
 
-		if (!visited.includes(tempNode) && tempNode[0] !== null) {
-			visited.push([tempNode[0], tempNode[1] + 1]);
-			nodeArr = [
-				...nodeArr,
-				[tempNode[0].left, tempNode[1] + 1],
-				[tempNode[0].right, tempNode[1] + 1],
-			];
-		}
-	}
+    if (!visited.includes(tempNode) && tempNode[0] !== null) {
+      visited.push([tempNode[0], tempNode[1] + 1]);
+      nodeArr = [
+        ...nodeArr,
+        [tempNode[0].left, tempNode[1] + 1],
+        [tempNode[0].right, tempNode[1] + 1],
+      ];
+    }
+  }
 
-	return visited;
+  return visited;
 }
