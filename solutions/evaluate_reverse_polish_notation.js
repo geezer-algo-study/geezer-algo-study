531dcd03d0260049566b93660039ac8fb0541f26
Salvation_sub <kyman19940214@gmail.com>
2024-10-31 20:30:26 +0900

    [2024.10.31] 김호균 evaluate reverse polush notation SOLVED

---
+++ solutions/evaluate_reverse_polish_notation.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  const stack = [];
+  const operators = ["+", "-", "*", "/"];
+
+  for (const token of tokens) {
+    if (!operators.includes(token)) {
+      stack.push(Number(token));
+    } else {
+      const b = stack.pop();
+      const a = stack.pop();
+      switch (token) {
+        case "+":
+          stack.push(a + b);
+          break;
+        case "-":
+          stack.push(a - b);
+          break;
+        case "*":
+          stack.push(a * b);
+          break;
+        case "/":
+          stack.push(Math.trunc(a / b));
+          break;
+      }
+    }
+  }
+
+  return stack.pop();
+};
